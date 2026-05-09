75e09afb0765c973fbab789160cb8db980668817
Adri0205 <adrianaflores20501@gmail.com>
2026-05-09 15:55:50 -0600

    soporte para variables de entorno

--- WEB/src/api/axios.ts
+++ WEB/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL as string,
 });
 
 // Adjunta el token JWT en cada petición si existe
